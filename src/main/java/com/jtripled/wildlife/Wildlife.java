c011a1074bc6f5c88154afdd7480dff3e5641c68
jtripled <jtripled@192.168.86.14>
2018-01-25 14:39:41 -0600

    tweaked registry system

--- src/main/java/com/jtripled/wildlife/Wildlife.java
+++ src/main/java/com/jtripled/wildlife/Wildlife.java
@@ -20,6 +20,8 @@ public class Wildlife extends ModBase
     @Mod.Instance(Wildlife.ID)
     public static Wildlife INSTANCE;
     
+    public static final Registry REGISTRY = new WildlifeRegistry();
+    
     public static final String ID = "wildlife";
     public static final String NAME = "Wildlife";
     public static final String VERSION = "1.0";
@@ -43,8 +45,8 @@ public class Wildlife extends ModBase
     }
     
     @Override
-    public Registry createRegistry()
+    public Registry getRegistry()
     {
-        return new WildlifeRegistry();
+        return REGISTRY;
     }
 }
